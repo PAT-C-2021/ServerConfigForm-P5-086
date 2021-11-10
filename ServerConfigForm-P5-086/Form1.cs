7318a262f1cc167df7b0c26c3c817deadd6f7e21
Abdil Tegar Arifin <abdiltegar12@gmail.com>
2021-11-10 12:41:24 +0700

    first push

---
+++ ServerConfigForm-P5-086/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.ServiceModel;
+using ServiceMtk_P1_20190140086;
+
+
+namespace ServerConfigForm_P5_086
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost Host;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOn_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+
+            labelOnOrOff.Text = "Server ON";
+            labelKet.Text = "Klik OFF untuk mematikan server";
+            Host.Open();
+            buttonOn.Enabled = false;
+            buttonOff.Enabled = true;
+        }
+
+        private void buttonOff_Click(object sender, EventArgs e)
+        {
+            buttonOn.Enabled = true;
+            buttonOff.Enabled = false;
+            Host.Close();
+            labelOnOrOff.Text = "Server OFF";
+            labelKet.Text = "Klik ON untuk menjalankan server";
+        }
+    }
+}
